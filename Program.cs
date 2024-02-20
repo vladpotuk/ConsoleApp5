8a862e7e9f46436202d0f6ef99a2d3482de1a40c
vladpotuk <vladpotuk01@gmail.com>
2024-02-20 19:37:39 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введіть число A: ");
+        int A = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Введіть число B (B > A): ");
+        int B = Convert.ToInt32(Console.ReadLine());
+
+        
+        for (int i = A; i <= B; i++)
+        {
+            
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine(); 
+        }
+    }
+}
